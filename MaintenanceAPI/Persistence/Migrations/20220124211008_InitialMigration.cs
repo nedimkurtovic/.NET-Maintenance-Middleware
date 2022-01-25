4eff71b47840aa2788cd27ebf987a2ecc87524f6
Nedim Kurtović <nerdyko@hotmail.com>
2022-01-25 18:53:40 +0100

    added initial migration

---
+++ MaintenanceAPI/Persistence/Migrations/20220124211008_InitialMigration.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MaintenanceAPI.Persistence.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Maintenances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Enabled = table.Column<bool>(type: "bit", nullable: false),
+                    AllAffected = table.Column<bool>(type: "bit", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maintenances", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MaintenanceEndpoints",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Controller = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RouteTemplate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MaintenanceId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaintenanceEndpoints", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MaintenanceEndpoints_Maintenances_MaintenanceId",
+                        column: x => x.MaintenanceId,
+                        principalTable: "Maintenances",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MaintenanceEndpoints_MaintenanceId",
+                table: "MaintenanceEndpoints",
+                column: "MaintenanceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MaintenanceEndpoints");
+
+            migrationBuilder.DropTable(
+                name: "Maintenances");
+        }
+    }
+}
